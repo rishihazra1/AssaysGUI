ecf1f4c18166ce35dc315149f6facfce73579d7a
rishihazra1 <54484447+rishihazra1@users.noreply.github.com>
2021-08-21 14:33:16 -0700

    Global files

---
+++ input_validation.py
@@ -0,0 +1,31 @@
+def get_float(input_prompt):
+    while True:
+        input_holder = input(input_prompt)
+        try:
+            input_holder = float(input_holder)
+            return input_holder
+        except:
+            ValueError
+            if input_holder == "no":  # enables user to exit loops
+                return input_holder
+            print("Invalid input. Enter valid input of type: " + "float") 
+
+def get_int(input_prompt):
+    while True:
+        input_holder = input(input_prompt)
+        try:
+            input_holder = int(input_holder)
+            return input_holder
+        except:
+            ValueError
+            print("Invalid input. Enter valid input of type: " + "int")
+            
+def get_y_or_no(input_prompt):
+    while True:
+        inupt_holder = input(input_prompt + " (y/n)\n")
+        if inupt_holder == "y" or inupt_holder == "n":
+            break
+        else:
+            print("Invalid input. Enter y or n.")
+    return inupt_holder
+
