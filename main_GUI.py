1b503014c6901eb1c9bf316408b9b1d2319d5e8c
rishihazra1 <hazrarish@gmail.com>
2021-08-21 15:03:14 -0700

    create full screen window

--- main_GUI.py
+++ main_GUI.py
@@ -2,4 +2,9 @@ import tkinter
 from BradfordAssay import bradford_assay
 from MichaelisMenten import michaelis_menten_plotter, individual_file_plotter
 
-main = tkinter.Toplevel
+window = tkinter.Tk()
+window.title('Assay Calculator, Rishi Hazra')
+width= window.winfo_screenwidth()               
+height= window.winfo_screenheight()               
+window.geometry("{0}x{1}+0+0".format(window.winfo_screenwidth(), window.winfo_screenheight()))
+window.mainloop()
