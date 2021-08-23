2d815603560b30fe6bda694e3ae1a6d522cf83b3
Rishi Hazra <hazrarish@gmail.com>
2021-08-22 17:08:44 -0700

    read inputs in GUI

--- main_GUI.py
+++ main_GUI.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import tkinter.font
+
+from matplotlib.pyplot import switch_backend
 import michaelis_menten_plotter
 import simple_statistics
 import math
@@ -37,11 +39,13 @@ class Home(tk.Frame):
 
         mm_btn = tk.Button(self, text="Michaelis-Menten", 
                             command=lambda: controller.show_frame("MichaelisMenten"), 
-                            fg='blue', bg='light grey', height=3, width=20, compound="c", font=("Helevicta", 30))  # mm = michaelis-menten
+                            fg='blue', bg='light grey', height=3, width=20, compound="c", 
+                            font=("Helevicta", 30))  # mm = michaelis-menten
         mm_btn.place(x=400, y=450)
         ba_btn = tk.Button(self, text="Bradford Assay",
                             command=lambda: controller.show_frame("BradfordAssay"), 
-                            fg='purple', bg='light grey', height=3, width=20, compound="c", font=("Helevicta", 30))  # ba = bradford assay
+                            fg='purple', bg='light grey', height=3, width=20, compound="c", 
+                            font=("Helevicta", 30))  # ba = bradford assay
         ba_btn.place(x=400, y=100)
         mm_btn.pack(pady=20)
         ba_btn.pack(pady=15)
@@ -53,12 +57,62 @@ class MichaelisMenten(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         label = tk.Label(self, text="Michaelis-Menten", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=25)
+        label.pack(side="top", fill="x", pady=65)
         home_button = tk.Button(self, text="HOME",
                            command=lambda: controller.show_frame("Home"), bg="sky blue", fg="black", font=("Helevicta", 15))
         home_button.place(height=75, width=75)
-        run_mm_plotter = tk.Button(self, text="Start Michaelis-Menten Assay Calculator", command=lambda: michaelis_menten_plotter.michaelis_menten)
-        run_mm_plotter.pack()
+        extinction_input = tk.Entry(self, bg="light grey", fg="black", bd="3", font=("Helevicta", 17)) 
+        extinction_label = tk.Label(self, text="Molar Extinction Coefficient (of substrate): ", font=("Helevicta", 17))
+        extinction_label.pack()
+        extinction_input.pack(pady=5)
+        concentration_input = tk.Entry(self, bg="light grey", fg="black", bd="3", font=("Helevicta", 17)) 
+        concentration_label = tk.Label(self, text="Enzyme Concentration (in µM): ", font=("Helevicta", 17))
+        concentration_label.pack()
+        concentration_input.pack(pady=5)
+        data_tracker_status = tk.Label(self, text="Off", font=("Helevicta", 12))
+        data_tracker_value=tk.IntVar()
+        data_tracker_box = tk.Checkbutton(self, text='In-built Data Tracker', font=("Helevicta", 14), variable=data_tracker_value, onvalue=1, 
+                                            offvalue=0, command=lambda: MichaelisMenten.switch_display(self, 
+                                            data_tracker_value.get(), data_tracker_status))
+        data_tracker_box.pack()
+        data_tracker_status.pack()
+        output_text = tk.Label(self, text="", font=("Helevicta", 14))
+        run_mm_plotter = tk.Button(self, text="Start Michaelis-Menten Assay Calculator", bg='light green', font=("Helevicta", 15),
+                                     command=lambda: MichaelisMenten.internal_michaelis_menten(self, extinction_input.get(), 
+                                     concentration_input.get(), data_tracker_value.get(), output_text))
+        run_mm_plotter.pack(pady=10)
+        output_text.pack()  
+
+    def switch_display(master, value, label):
+        if value == 1:
+            label.configure(text="On")
+        elif value == 0:
+            label.configure(text="Off")
+
+    def internal_michaelis_menten(master, extinction, concentration, use_data_tracker, output_text):
+        valid_concentration = False
+        valid_extinction = False
+        try:
+            molar_extinction = float(extinction)
+            valid_extinction = True
+        except ValueError:
+            output_text.configure(text="Invalid extinction coefficient. Try again.")
+        try:
+            enzyme_concentration = float(concentration)
+            valid_concentration = True
+        except ValueError:
+            output_text.configure(text="Invalid enzyme concentration. Try again.")
+        if valid_extinction is True and valid_concentration is True:
+            print("THIS LOCATION")
+            output_text.configure(text="Loading Enzyme Kinetics Calculator")
+            if use_data_tracker == 1:
+                print("ALSO HERE")
+                output_text.configure(text="Loading In-Built Data Tracker")            
+         #   michaelis_menten_plotter.michaelis_menten(master, extinction, concentration, use_data_tracker, output_text)
+
+
+
+
 
 class BradfordAssay(tk.Frame):
 
@@ -85,7 +139,10 @@ class BradfordAssay(tk.Frame):
         absorption_label.pack()
         absorption_input.pack(pady=5)
         output_text = tk.Label(self, text="", font=("Helevicta", 14))
-        run_ba = tk.Button(self, text="Get Protein Concentration", bg='light green', font=("Helevicta", 15), command=lambda: BradfordAssay.internal_bradford_assay(self, protein_amount_input.get(), dilution_input.get(), absorption_input.get(), output_text))
+        run_ba = tk.Button(self, text="Get Protein Concentration", bg='light green', font=("Helevicta", 15), 
+                                command=lambda: BradfordAssay.internal_bradford_assay(self, protein_amount_input.get(), 
+                                dilution_input.get(), absorption_input.get(), output_text))
+        
         run_ba.pack(pady=10)  
         output_text.pack()      
         
@@ -102,7 +159,9 @@ class BradfordAssay(tk.Frame):
         except ValueError:
             print("ERROR -- dilution")
             output_text.configure(text="Invalid dilution entered. Try again.")        
-        baselines = [['20', '0', '1.000', '1.258', '1.155'], ['15', '5', '0.750', '1.083', '1.098'], ['10', '10', '0.500', '0.728', '0.713'], ['5', '15', '0.250', '0.422', '0.402'], ['2.5', '17.5', '0.125', '0.223', '0.214'], ['0', '20', '0.000', '0', '-0.005']]
+        baselines = [['20', '0', '1.000', '1.258', '1.155'], ['15', '5', '0.750', '1.083', '1.098'], 
+                        ['10', '10', '0.500', '0.728', '0.713'], ['5', '15', '0.250', '0.422', '0.402'], 
+                        ['2.5', '17.5', '0.125', '0.223', '0.214'], ['0', '20', '0.000', '0', '-0.005']]
         print("protein_used: " + str(protein_used))
         print("dilution: " + str(dilution))
         x_terms, y_terms = baseline.calculate_baselines(baselines)
