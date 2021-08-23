04bc1a558667a579e5d0797f772bd3d5faf674c9
Rishi Hazra <hazrarish@gmail.com>
2021-08-22 17:30:54 -0700

    compile_GUI without conflicts

--- bradford_assay.py
+++ bradford_assay.py
@@ -4,9 +4,34 @@ import simple_statistics
 import math
 import tkinter as tk
 
-def bradford_assay(master, absorption):
-    print("absorption: " + str(absorption))
-    path = baseline.set_default_baseline()
+def bradford_assay_main(master, protein_used, dilution, absorption, output_text):
+    try:
+            protein_used = float(protein_used)
+    except ValueError:
+        print("ERROR -- protein_amount")
+        output_text.configure(text="Invalid protein amount entered. Try again.")
+    try:
+        dilution = float(dilution)
+    except ValueError:
+        print("ERROR -- dilution")
+        output_text.configure(text="Invalid dilution entered. Try again.")        
+    baselines = [['20', '0', '1.000', '1.258', '1.155'], ['15', '5', '0.750', '1.083', '1.098'], ['10', '10', '0.500', '0.728', '0.713'], ['5', '15', '0.250', '0.422', '0.402'], ['2.5', '17.5', '0.125', '0.223', '0.214'], ['0', '20', '0.000', '0', '-0.005']]
+    print("protein_used: " + str(protein_used))
+    print("dilution: " + str(dilution))
+    x_terms, y_terms = baseline.calculate_baselines(baselines)
+    dilution_factor = (protein_used/20)*dilution
+    a, b, c = simple_statistics.get_best_fit_line(x_terms, y_terms)  # values generated through numpy polyfit
+    r_squared = simple_statistics.get_r_squared(x_terms, y_terms, a, b, c)
+    print("a = " + str(a) + "\nb = " + str(b) + "\nc = " + str(c))
+    try:   
+        concentration = ((-b + math.sqrt(b**2 - 4*a*c + 4*a*float(absorption)))/(2*a))/dilution_factor
+        print("protein concentration (mg/mL): " + str(concentration))
+        output_text.configure(text=str(absorption) + "A →  " + str(concentration) + " mg/mL\n" + "r² = " + str(r_squared))     
+    except ValueError:
+        print("ERROR -- absorbance")
+        output_text.configure(text="Invalid absorption. Try again.") 
+
+    #  features below are to be implemented
     '''
     use_default = input_validation.get_y_or_n("Use default baselines?")
     if use_default == "n":
@@ -14,12 +39,6 @@ def bradford_assay(master, absorption):
         quit()
     '''
 
-    baselines = [['20', '0', '1.000', '1.258', '1.155'], ['15', '5', '0.750', '1.083', '1.098'], ['10', '10', '0.500', '0.728', '0.713'], ['5', '15', '0.250', '0.422', '0.402'], ['2.5', '17.5', '0.125', '0.223', '0.214'], ['0', '20', '0.000', '0', '-0.005']]
-    print("baselines: " + str(baselines))
-    x_terms, y_terms = baseline.calculate_baselines(baselines)
-    print(x_terms, y_terms)
-
-    print("\nProtein used: 5µL\n" + "Dilution of protein solution: 0.1")
     '''
     info_correct = input_validation.get_y_or_n("Is the above information correct?")
     if info_correct == "n":
@@ -31,27 +50,9 @@ def bradford_assay(master, absorption):
     dilution_factor = (protein_used/20)*dilution
     # print("dilution factor: " + str(dilution_factor))
     '''
-    protein_used = 5
-    dilution = 0.1
-    dilution_factor = (protein_used/20)*dilution
-    a, b, c = simple_statistics.get_best_fit_line(x_terms, y_terms)  # values generated through numpy polyfit
 
     '''
     a = -0.506713304184884  # values from Excel LINEST function
     b = 1.773611242973140
     c = 0.002712554653342
     '''
-
-    r_squared = simple_statistics.get_r_squared(x_terms, y_terms, a, b, c)
-    print("a = " + str(a) + "\nb = " + str(b) + "\nc = " + str(c))
-    while True:
-        try:   
-            concentration = ((-b + math.sqrt(b**2 - 4*a*c + 4*a*float(absorption)))/(2*a))/dilution_factor
-            print("protein concentration (mg/mL): " + str(concentration))
-            return concentration
-        except:
-            ValueError
-            error_message = tk.Label(master, text="Invalid input, try again.")
-            error_message.pack()
-            
-    
